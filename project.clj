5004b8d6c519940f018940cb309a0ea013993903
Rory 'Howl' Howe (Laptop) <archmagekaioso@gmail.com>
2015-05-29 19:13:00 -0700

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject clofbot "0.0.1-SNAPSHOT"
+  :description "Funkagroovitalizer written in clojure!"
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :profiles {:dev {:dependencies [[midje "1.5.1"]]}})
+  
